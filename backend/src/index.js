5504534480fddb91f86510cc481c1524f0560b19
Samsil Arefin <samsilarefin000@gmail.com>
2025-04-18 16:55:10 +0200

    add auth.routes.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,9 @@
 import express from "express";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
+
+app.use("/api/auth", authRoutes);
 
 app.listen(5001, () => {
   console.log("Server is running on http://localhost:5001");
