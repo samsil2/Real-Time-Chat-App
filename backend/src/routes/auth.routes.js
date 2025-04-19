92ced7955707ffc5ba55d060eb0f0097078220dd
Samsil Arefin <samsilarefin000@gmail.com>
2025-04-19 14:41:54 +0200

    update auth.routes..js

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -7,7 +7,7 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
-router.put("/update-profile",updateProfile);
+router.put("/update-profile", protectRoute, updateProfile);
 
 export default router;
 
