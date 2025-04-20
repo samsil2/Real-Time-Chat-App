3d86e5df05b8af86f40aa18f1ec51ea70eedfd23
Samsil Arefin <samsilarefin000@gmail.com>
2025-04-20 02:56:03 +0200

    add cookie-parser in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,13 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
 
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 const PORT = process.env.PORT;
 
