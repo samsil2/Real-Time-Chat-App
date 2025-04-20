2e8e7588a2caf7784f6dbcc771d4a839bb0e6455
Samsil Arefin <samsilarefin000@gmail.com>
2025-04-20 03:04:42 +0200

    import protectRoute in auth.routes.js

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { signup, login, logout } from "../controllers/auth.controllers.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
