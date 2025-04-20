d19da7bdb71badeb54b30f1458ef8b5465ddf3cd
Samsil Arefin <samsilarefin000@gmail.com>
2025-04-20 16:50:09 +0200

    implement getMessages

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -4,5 +4,6 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
 
 export default router;
