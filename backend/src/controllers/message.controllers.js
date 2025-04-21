ad51404056fc549a96e63139c4d110e454650ede
Samsil Arefin <samsilarefin000@gmail.com>
2025-04-21 03:01:13 +0200

    send message added - except socket.io

--- backend/src/controllers/message.controllers.js
+++ backend/src/controllers/message.controllers.js
@@ -104,3 +104,34 @@ export const getMessages = async (req, res) => {
   }
 };
 
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      // Upload base64 image to cloudinary
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    //later socket.io(real-time functionality) will be added
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
