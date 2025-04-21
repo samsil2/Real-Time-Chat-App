4c34a28a7a64a66418ef7e535da70d7630c1490e
Samsil Arefin <samsilarefin000@gmail.com>
2025-04-22 01:51:50 +0200

    created pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import Navbar from './components/navbar.jsx'
+import {Route, Routes} from 'react-router-dom'
+import HomePage from './pages/HomePage.jsx'
+import SignUpPage from './pages/SignUpPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import SettingsPage from './pages/SettingsPage.jsx'
+import ProfilePage from './pages/ProfilePage.jsx'
 
 const App = () => {
   return (
-    <div className='text-red-500'>
-     Hello 
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/signup" element={<SignUpPage/>} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/settings" element={<SettingsPage/>} />
+        <Route path="/profile" element={<ProfilePage/>} />
+        
+      </Routes>
+
+
     </div>
   )
 }
