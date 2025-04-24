5764b5a4a3faca1e94fbabdd2322294d9dafbb0e
Samsil Arefin <samsilarefin000@gmail.com>
2025-04-24 02:30:19 +0200

    update signup page

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAuthStore } from '../store/useAuthStore';
 
 const SignUpPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+
+  const { signup, isSigningUp } = useAuthStore();
+
+
   return (
     <div>
       signup
