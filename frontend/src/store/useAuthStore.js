b5873b741599cac138027849b9dcf0c0cb10b83d
Samsil Arefin <samsilarefin000@gmail.com>
2025-04-25 02:57:40 +0200

    logout option

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -40,6 +40,17 @@ const useAuthStore = create((set) => ({
   }
  },
 
+ logout: async () => {
+  try {
+    await axiosInstance.post("/auth/logout");
+    set({ authUser: null });
+    toast.success("Logged out successfully");
+    
+  } catch (error) {
+    toast.error(error.response.data.message);
+  }
+},
+
 
 }));
 
