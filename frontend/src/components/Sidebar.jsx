1cb6feb5745c91701a5de1b6674f1bd51cfd0fad
Samsil Arefin <samsilarefin000@gmail.com>
2025-05-01 01:08:55 +0200

    SidebarSkeleton

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useChatStore } from "../store/useChatStore";
 
 const Sidebar = () => {
+  const { getUsers, users, selectedUser, setSelectedUser, isUsersLoading } = useChatStore();
+
+  const onlineUsers = [];
+
+  useEffect(() => {
+    getUsers();
+  }, [getUsers]);
+
+  if (isUsersLoading) return <SidebarSkeleton />;
   return (
     <div>
       Sidebar
