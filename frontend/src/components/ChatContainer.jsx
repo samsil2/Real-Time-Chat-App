7118c3f4db8169a86a7b982294506dc578905332
Samsil Arefin <samsilarefin000@gmail.com>
2025-05-22 12:23:41 +0200

    update

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -12,14 +12,16 @@ const ChatContainer = () => {
     getMessages(selectedUser._id);
   }, [selectedUser._id, getMessages]);
 
-  if (isMessagesLoading) return(
-    <div className="flex-1 flex flex-col overflow-auto">
-      <ChatHeader />
-      <MessageSkeleton />
-      <MessageInput />
-
-    </div>
-  )
+  if (isMessagesLoading){
+    return(
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+  
+      </div>
+    )
+  }
 
   return (
     <div className="flex-1 flex flex-col overflow-auto">
