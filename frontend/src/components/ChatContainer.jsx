f1a7ee5ce0a1d9bc87b64559b270f7ac20ecc5c9
Samsil Arefin <samsilarefin000@gmail.com>
2025-05-22 12:04:38 +0200

    chat container updated

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -3,6 +3,7 @@ import { useChatStore } from "../store/useChatStore";
 import { useEffect } from 'react';
 import ChatHeader from './ChatHeader';
 import MessageInput from './MessageInput';
+import MessageSkeleton from './skeletons/MessageSkeleton';
 
 const ChatContainer = () => {
   const {messages, getMessages, isMessagesLoading,selectedUser} = useChatStore();
@@ -11,7 +12,14 @@ const ChatContainer = () => {
     getMessages(selectedUser._id);
   }, [selectedUser._id, getMessages]);
 
-  if (isMessagesLoading) return <div>Loading... </div>
+  if (isMessagesLoading) return(
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+      <MessageSkeleton />
+      <MessageInput />
+
+    </div>
+  )
 
   return (
     <div className="flex-1 flex flex-col overflow-auto">
