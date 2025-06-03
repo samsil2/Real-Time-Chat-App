8e675daf1a9f581c7d572dae2c2ba2866aee08a0
Samsil Arefin <samsilarefin000@gmail.com>
2025-06-04 01:45:15 +0200

    subscribeToMessages

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
+import useAuthStore  from "./useAuthStore";
 
 export const useChatStore = create((set,get) => ({
   messages: [],
@@ -43,7 +44,24 @@ export const useChatStore = create((set,get) => ({
     }
   },
 
-  //optime later
+  subscribeToMessages: () => {
+    const { selectedUser } = get();
+    if (!selectedUser) return;
+
+    const socket = useAuthStore.getState().socket;
+
+    socket.on("newMessage", (newMessage) => {
+      const isMessageSentFromSelectedUser = newMessage.senderId === selectedUser._id;
+      if (!isMessageSentFromSelectedUser) return;
+
+      set({
+        messages: [...get().messages, newMessage],
+      });
+    });
+  },
+
+
+
   setSelectedUser: (selectedUser) => set({ selectedUser }),
 
 }))
