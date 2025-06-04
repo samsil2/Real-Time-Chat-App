50dac0ebebe072f9757f64e7de0dd837856b8471
Samsil Arefin <samsilarefin000@gmail.com>
2025-06-04 03:09:17 +0200

    update for dev

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,12 +7,14 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.route.js";
 import cors from"cors";
 import { app, server } from "./lib/socket.js";
+import path from "path";
 
 
 
 dotenv.config();
 
-
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -27,10 +29,19 @@ app.use(
 
 
 
-const PORT = process.env.PORT;
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
