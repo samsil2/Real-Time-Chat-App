816fe2418b5d7112c310d7bfdbcccdaf4ba498c1
Samsil Arefin <samsilarefin000@gmail.com>
2025-06-04 02:02:13 +0200

    unsubscribeFromMessages

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -60,6 +60,11 @@ export const useChatStore = create((set,get) => ({
     });
   },
 
+  unsubscribeFromMessages: () => {
+    const socket = useAuthStore.getState().socket;
+    socket.off("newMessage");
+  },
+
 
 
   setSelectedUser: (selectedUser) => set({ selectedUser }),
